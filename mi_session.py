11937e947cebd691f0de4ed7ebadc15b8a2ffb49
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-07-09 17:36:06 -0400

    Add files via upload

--- mi_session.py
+++ mi_session.py
@@ -8,4 +8,41 @@ def build_list():
         li.append((nom,age))
         cont = int(input("Ajouter un autre etudiant ? (1/0) "))
     return print(li)
-build_list()
+def switch(liste,i,j):
+    print(liste)
+    c = liste[i]
+    liste[i] = liste[j]
+    liste[j] = c
+    return print(liste)
+ 
+def bubbleSort(li):
+     while (True):
+        permutation = 0
+        print(li)
+        for k in range(len(li)-1):
+            if li[k]>li[k+1]:
+                switch(li,k,k+1)
+                permutation+=1
+        if (permutation==0):
+            return li
+        
+def selectionSort(l):
+    while (True):
+        permutation = 0
+        print(l)
+        min = l[0][1]
+        for i in range(len(l)-1):
+            if min > l[i][i+1]:
+                min = l[i][i+1]
+                switch(l,i,i+1)
+                permutation+=1
+        if (permutation==0):
+            return l
+
+lis = [("Viny", 34),("Ryan", 43),("Tity", 31),("Antony", 27),
+       ("Calvin", 39),("Lilian", 27)] 
+#mes appel de fonction pour test decocher le commentaire par fonction pour verifier
+#selectionSort(lis)
+bubbleSort(lis)      
+switch(lis,1,2)
+#build_list()
