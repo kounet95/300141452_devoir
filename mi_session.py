1b4bd5673b39269ce607ef7b27ece8cb3da7ad9b
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-07-09 14:29:36 -0400

    kounet

--- mi_session.py
+++ mi_session.py
@@ -1,1 +1,8 @@
-print("kounet")
+def build_list():
+    cont = int(input("Ajouter un autre etudiant ? (1/0) "))
+    while cont != 0:
+        nom = str(input("entre un nom:"))
+        age = int(input("entre lage:"))
+        print("nom:", " ", nom,"\n", "Age:",age)
+        cont = int(input("Ajouter un autre etudiant ? (1/0) "))
+build_list()  
